4db4de8c91ce9905141cca9c9957f892e1dcb0b3
Muhammad Zaman <mnez.creations@gmail.com>
2026-01-27 20:18:18 -0800

    Created links data for all socials and other networking links

---
+++ src/data/links.tsx
@@ -0,0 +1,412 @@
+const links = [
+    {
+        name: "Upscrolled",
+        link: "https://share.upscrolled.com/en/user/ae4d87be-ad37-4841-a224-3f6afd5b49f7",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Pixelfed",
+        link: "https://pixelfed.social/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Bluesky",
+        link: "https://bsky.app/profile/madmanserenade.bsky.social",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Skylight",
+        link: "https://skylight.social/profile/did:plc:khksxwfke44u7qscfliytvnm",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Passes",
+        link: "https://www.passes.com/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Skool",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "TikTok",
+        link: "https://www.tiktok.com/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "YouTube",
+        link: "https://www.youtube.com/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Instagram",
+        link: "https://www.instagram.com/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Mastodon",
+        link: "https://mastodon.social/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "GitHub",
+        link: "https://github.com/MadManSerenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Venmo",
+        link: "https://venmo.com/u/MadManSerenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Cookpad",
+        link: "https://cookpad.com/eng/users/115291467",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Spotify",
+        link: "https://open.spotify.com/user/mercmaverick",
+        icon: "",
+        active: true
+    },
+    {
+        name: "SubStack",
+        link: "https://substack.com/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Telegram",
+        link: "https://t.me/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "SoundCloud",
+        link: "https://soundcloud.com/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "SnapChat",
+        link: "https://www.snapchat.com/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Threads",
+        link: "https://www.threads.com/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Facebook",
+        link: "https://www.facebook.com/profile.php?id=100095184946094",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Discord",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Voiced",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Twitch",
+        link: "https://m.twitch.tv/madmanserenade/home",
+        icon: "",
+        active: true
+    },
+    {
+        name: "3five",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Odysee",
+        link: "https://odysee.com/@MadManSerenade:5",
+        icon: "",
+        active: true
+    },
+    {
+        name: "LinkedIn",
+        link: "https://linkedin.com/in/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Clubhouse",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Pintrest",
+        link: "https://www.pinterest.com/madmanserenade/",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Fanbase",
+        link: "https://www.fanbase.app/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Twitter",
+        link: "https:/x.com/madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Lemon8",
+        link: "https://www.lemon8-app.com/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Slack",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Nextdoor",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Kick",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Favorited",
+        link: "https://fav.gg/@madmanserenade",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Yzzy",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Are.na",
+        link: "https://www.are.na/muhammad-zaman/channels",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Likee",
+        link: "https://likee.video/p/LmP XoR",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Scoopz",
+        link: "https://scoopzapp.com/@370798302",
+        icon: "",
+        active: true
+    },
+    {
+        name: "ezze",
+        link: "https://m.ezze.live/refer/rjpw?code=2W8E5",
+        icon: "",
+        active: true
+    },
+    {
+        name: "Zora",
+        link: "https://zora.co/@madmanserenade/activity",
+        icon: "",
+        active: true
+    },
+    {
+        name: "TripBFF",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Reinbow",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Fide",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "nuri lounge",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "MeWe",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Rocky",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "OnceUp",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Peeps",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Buddy",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Luvi",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Dizkover",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Boo",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Ramdam",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Yubo",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Clapper",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Swarm",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Qudo",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "BIGOLI",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Citizen",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "PI.FYI",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "rednote",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Reddit",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Relay",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "MOVZ",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Equa.ls",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Culture Connection",
+        link: "",
+        icon: "",
+        active: false
+    },
+    {
+        name: "Dreemz",
+        link: "",
+        icon: "",
+        active: false
+    },
+]
+
+export { links }
