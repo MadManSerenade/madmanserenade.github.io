1e471669f19ff4dc19b6ea6d449bcda5ed0f62df
Muhammad Zaman <mnez.creations@gmail.com>
2026-04-13 17:38:54 -0700

    Cleaning up home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,15 @@
+// import { Contact } from "@/components/contact";
 import { Experience } from "@/components/experience";
+// import { Gallery } from "@/components/gallery";
 import { Hero } from "@/components/hero";
 import { Projects } from "@/components/projects";
+// import { Services } from "@/components/services";
 
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
     component: Home
 })
-
-// import { Hero } from "./components/hero"
-// import { Services } from "./components/services"
-// import { Projects } from "./components/projects"
-// import { Gallery } from "./components/gallery"
-// import { Experience } from "./components/experience"
-// import { Contact } from "./components/contact"
-// import { Footer } from "./components/footer"
 
 export function Home() {
   return (
