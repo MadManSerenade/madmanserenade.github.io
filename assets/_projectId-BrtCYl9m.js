ba54c99608b7f9658dbffde6ecae0b3857339b5a
Muhammad Zaman <mnez.creations@gmail.com>
2026-05-10 02:02:16 -0700

    Attempting update after turning projects $projectID pages back on...

---
+++ assets/_projectId-BrtCYl9m.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CE_4wDq_.js";function e(){return o.jsx("div",{children:'Hello "/projects/$projectId"!'})}export{e as component};
---
+++ assets/index-Cuz6V0zF.js
@@ -0,0 +1,1 @@
+import{j as o,P as t}from"./index-CE_4wDq_.js";function e(){return o.jsx(o.Fragment,{children:o.jsx(t,{})})}export{e as component};
