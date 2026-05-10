88305efa27ea80d83498d36bf0c10a971071b911
Muhammad Zaman <mnez.creations@gmail.com>
2026-05-10 02:25:54 -0700

    Implementing hash routing for github pages functional access to pages beyond index.html

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { createRoot } from "react-dom/client"
 import "./globals.css"
 import "./utils.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
-import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { RouterProvider, createHashHistory, createRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
 
-const router = createRouter({ routeTree })
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory })
 
 declare module "@tanstack/react-router" {
   interface Register {
