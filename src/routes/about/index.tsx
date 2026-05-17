cdcbbcaf996d844c370509d451ba20f106b1cf45
Muhammad Zaman <mnez.creations@gmail.com>
2026-05-16 19:33:23 -0700

    Linking mdx for each page accordingly

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,8 +1,4 @@
-// import { useState, useEffect } from "react";
-// import UnderConstruction from "@/components/under-construction"
 import { createFileRoute } from "@tanstack/react-router"
-// import { readFileSync } from "fs"
-// import Markdown from "react-markdown"
 import {MDXProvider} from '@mdx-js/react';
 import About from "@/markdown/about.mdx"
 
--- src/routes/community/index.tsx
+++ src/routes/community/index.tsx
@@ -1,5 +1,6 @@
-import UnderConstruction from "@/components/under-construction"
 import { createFileRoute } from "@tanstack/react-router"
+import {MDXProvider} from '@mdx-js/react';
+import Community from "@/markdown/community.mdx"
 
 export const Route = createFileRoute("/community/")({
   component: RouteComponent
@@ -7,11 +8,8 @@ export const Route = createFileRoute("/community/")({
 
 function RouteComponent() {
   return (
-    <UnderConstruction>
-      <h1>MuhamMAD Navid E ZaMAN - MadManSerenade</h1>
-      <h3 className="text-3xl rtl:ml-0">مَآ أَنتَ بِنِعْمَةِ رَبِّكَ بِمَجْنُونٍۢ</h3>
-      <h2>Thou art not, by thy Sustainer's grace, a madman!</h2>
-      <h2>Quran 68:2</h2>
-    </UnderConstruction>
+    <MDXProvider>
+      <Community />
+    </MDXProvider>
   )
 }
